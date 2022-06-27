653fa5a6fb1efc09dbeaa31cb7b457a84f4856b1
Himanshu Bhardwaz <himanshu76200@gmail.com>
2022-06-28 00:04:28 +0530

    redirect working

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../db/client"
+
+export default async function API(req: NextApiRequest, res: NextApiResponse) {
+    const slug = req.query["slug"];
+
+    if (!slug || typeof slug !== "string") {
+        res.statusCode = 404;
+
+        res.send(JSON.stringify({ message: "Not Found" }))
+
+        return;
+    }
+
+    const data = await prisma?.shortLink.findFirst({
+        where: {
+            slug: {
+                equals: slug
+            }
+        }
+    })
+
+    if (!data) {
+        res.statusCode = 404;
+
+        res.send(JSON.stringify({ message: "Not Found" }))
+
+        return;
+    }
+
+    return res.redirect(data.url);
+
+};
