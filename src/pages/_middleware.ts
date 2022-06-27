2bc1957bbd8036cc0385feafee8004015d78807c
Himanshu Bhardwaz <himanshu76200@gmail.com>
2022-06-28 00:44:14 +0530

    fixed redirects and cache

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,16 +1,21 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest, res: NextResponse) {
-    if (req.nextUrl.pathname.startsWith("/api/get-url")) {
-        return
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+    if (
+        req.nextUrl.pathname.startsWith("/api/") ||
+        req.nextUrl.pathname === "/"
+    ) {
+        return;
     }
-
     const slug = req.nextUrl.pathname.split("/").pop();
 
-    const data = await (await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`)).json();
+    const slugFetch = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
+    if (slugFetch.status === 404) {
+        return NextResponse.redirect(req.nextUrl.origin);
+    }
+    const data = await slugFetch.json();
 
     if (data?.url) {
         return NextResponse.redirect(data.url);
     }
-
 }
--- src/pages/api/get-url/[slug].ts
+++ src/pages/api/get-url/[slug].ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "../../../db/client"
+
+import { prisma } from "../../../db/client";
 
 export default async function API(req: NextApiRequest, res: NextApiResponse) {
     const slug = req.query["slug"];
@@ -7,26 +8,33 @@ export default async function API(req: NextApiRequest, res: NextApiResponse) {
     if (!slug || typeof slug !== "string") {
         res.statusCode = 404;
 
-        res.send(JSON.stringify({ message: "Not Found" }))
+        res.send(JSON.stringify({ message: "pls use with a slug" }));
 
         return;
     }
 
-    const data = await prisma?.shortLink.findFirst({
+    const data = await prisma.shortLink.findFirst({
         where: {
             slug: {
-                equals: slug
-            }
-        }
-    })
+                equals: slug,
+            },
+        },
+    });
 
     if (!data) {
         res.statusCode = 404;
 
-        res.send(JSON.stringify({ message: "Not Found" }))
+        res.send(JSON.stringify({ message: "slug not found" }));
 
         return;
     }
 
-    return res.json({ data });
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Cache-Control",
+        "s-maxage=1000000000, stale-while-revalidate"
+    );
+
+    return res.json(data);
 };
